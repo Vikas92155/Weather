e29a7b20017fc50b025a7d3f7b5f8301a232c133
Vikas Gupta <vikasgupta.92155@gmail.com>
2017-08-22 22:37:22 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,126 @@
+var x;
+var url;
+var city, country;
+var arr;
+var arr1 = [2];
+var astro = [1];
+var i,
+    j,
+    forecast1 = [];
+forecast2 = [];
+forecast3 = [];
+forecast4 = [];
+forecast5 = [];
+forecast6 = [];
+forecast7 = [];
+
+function weather(event) {
+    event.preventDefault();
+    city = document.getElementById("city").value;
+    country = document.getElementById("country").value;
+    url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + city + "%2C%20I" + country + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    $.get(url, function(item1) {
+        x = item1;
+        arr = x.query.results.channel.lastBuildDate;
+        arr1[0] = x.query.results.channel.location.city;
+        arr1[1] = x.query.results.channel.location.country;
+        arr1[2] = x.query.results.channel.location.region;
+        $("#item1").text(arr); //current date and time display
+        $("#item2").text(arr1[0] + ","); //displays current location
+        $("#item3").text(arr1[1]);
+        $("#item4").text(arr1[2] + ",");
+        astro[0] = x.query.results.channel.astronomy.sunrise;
+        astro[1] = x.query.results.channel.astronomy.sunset;
+        $("#sunrise").text("Sunrise:" + astro[0]); //current day sunrise display
+        $("#sunset").text("Sunset:" + astro[1]); //currrent day sunset display
+        //Current weather display
+        forecast1[0] = x.query.results.channel.item.forecast[0].date;
+        forecast1[1] = x.query.results.channel.item.forecast[0].day;
+        forecast1[2] = x.query.results.channel.item.forecast[0].high;
+        forecast1[3] = x.query.results.channel.item.forecast[0].low;
+        forecast1[4] = x.query.results.channel.item.forecast[0].text;
+        $("#forecast02").text("Highest: " + (parseInt(forecast1[2]) - 32) * 0.5556 + "'C");
+        $("#forecast03").text("Lowest: " + (parseInt(forecast1[3]) - 32) * 0.5556 + "'C");
+        $("#forecast04").text(forecast1[4]);
+
+        //all rest six days weather display
+        //day 1
+        $("#rest").text("Weather For Next Six Days");
+        forecast2[0] = x.query.results.channel.item.forecast[1].date;
+        forecast2[1] = x.query.results.channel.item.forecast[1].day;
+        forecast2[2] = x.query.results.channel.item.forecast[1].high;
+        forecast2[3] = x.query.results.channel.item.forecast[1].low;
+        forecast2[4] = x.query.results.channel.item.forecast[1].text;
+        $("#forecast10").text(forecast2[0]);
+        $("#forecast11").text(forecast2[1]);
+        $("#forecast12").text("Highest: " + (parseInt(forecast2[2]) - 32) * 0.5556 + "'F");
+        $("#forecast13").text("Lowest: " + (parseInt(forecast2[3]) - 32) * 0.5556 + "'F");
+        $("#forecast14").text(forecast2[4]);
+
+        //day2
+
+        forecast3[0] = x.query.results.channel.item.forecast[2].date;
+        forecast3[1] = x.query.results.channel.item.forecast[2].day;
+        forecast3[2] = x.query.results.channel.item.forecast[2].high;
+        forecast3[3] = x.query.results.channel.item.forecast[2].low;
+        forecast3[4] = x.query.results.channel.item.forecast[2].text;
+        $("#forecast20").text(forecast3[0]);
+        $("#forecast21").text(forecast3[1]);
+        $("#forecast22").text("Highest: " + (parseInt(forecast3[2]) - 32) * 0.5556 + "'F");
+        $("#forecast23").text("Lowest: " + (parseInt(forecast3[3]) - 32) * 0.5556 + "'F");
+        $("#forecast24").text(forecast3[4]);
+
+        //day 3
+
+        forecast4[0] = x.query.results.channel.item.forecast[3].date;
+        forecast4[1] = x.query.results.channel.item.forecast[3].day;
+        forecast4[2] = x.query.results.channel.item.forecast[3].high;
+        forecast4[3] = x.query.results.channel.item.forecast[3].low;
+        forecast4[4] = x.query.results.channel.item.forecast[3].text;
+        $("#forecast30").text(forecast4[0]);
+        $("#forecast31").text(forecast4[1]);
+        $("#forecast32").text("Highest: " + (parseInt(forecast4[2]) - 32) * 0.5556 + "'F");
+        $("#forecast33").text("Lowest: " + (parseInt(forecast4[3]) - 32) * 0.5556 + "'F");
+        $("#forecast34").text(forecast4[4]);
+
+        //day 4
+
+        forecast5[0] = x.query.results.channel.item.forecast[4].date;
+        forecast5[1] = x.query.results.channel.item.forecast[4].day;
+        forecast5[2] = x.query.results.channel.item.forecast[4].high;
+        forecast5[3] = x.query.results.channel.item.forecast[4].low;
+        forecast5[4] = x.query.results.channel.item.forecast[4].text;
+        $("#forecast40").text(forecast5[0]);
+        $("#forecast41").text(forecast5[1]);
+        $("#forecast42").text("Highest: " + (parseInt(forecast5[2]) - 32) * 0.5556 + "'F");
+        $("#forecast43").text("Lowest: " + (parseInt(forecast5[3]) - 32) * 0.5556 + "'F");
+        $("#forecast44").text(forecast5[4]);
+
+        //day5
+
+        forecast6[0] = x.query.results.channel.item.forecast[5].date;
+        forecast6[1] = x.query.results.channel.item.forecast[5].day;
+        forecast6[2] = x.query.results.channel.item.forecast[5].high;
+        forecast6[3] = x.query.results.channel.item.forecast[5].low;
+        forecast6[4] = x.query.results.channel.item.forecast[5].text;
+        $("#forecast50").text(forecast6[0]);
+        $("#forecast51").text(forecast6[1]);
+        $("#forecast52").text("Highest: " + (parseInt(forecast6[2]) - 32) * 0.5556 + "'F");
+        $("#forecast53").text("Lowest: " + (parseInt(forecast6[3]) - 32) * 0.5556 + "'F");
+        $("#forecast54").text(forecast6[4]);
+
+        //day6
+
+        forecast7[0] = x.query.results.channel.item.forecast[6].date;
+        forecast7[1] = x.query.results.channel.item.forecast[6].day;
+        forecast7[2] = x.query.results.channel.item.forecast[6].high;
+        forecast7[3] = x.query.results.channel.item.forecast[6].low;
+        forecast7[4] = x.query.results.channel.item.forecast[6].text;
+        $("#forecast60").text(forecast7[0]);
+        $("#forecast61").text(forecast7[1]);
+        $("#forecast62").text("Highest: " + (parseInt(forecast7[2]) - 32) * 0.5556 + "'F");
+        $("#forecast63").text("Lowest: " + (parseInt(forecast7[3]) - 32) * 0.5556 + "'F");
+        $("#forecast64").text(forecast7[4]);
+    });
+
+};
---
+++ js/weather.js
@@ -0,0 +1,61 @@
+var x = document.getElementById("demo");
+var array1 = [];
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU";
+    document.getElementById("mapholder").innerHTML = "<img src='" + img_url + "'>";
+    var url = "http://maps.googleapis.com/maps/api/geocode/json?latlng="+position.coords.latitude+","+position.coords.longitude+"&sensor=true";
+    $.get(url,function(pos){
+        var par = pos;
+         var city1 = par.results[0].address_components[3].long_name;
+         var country1 = par.results[0].address_components[6].long_name;
+    var url1 = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22"+city1+"%2C%20I"+country1+"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    $.get(url1,function(posit){
+        var parse = posit;
+        array1[0] = parse.query.results.channel.lastBuildDate;
+        array1[1] = parse.query.results.channel.location.city;
+        array1[2] = parse.query.results.channel.location.region;
+        array1[3] = parse.query.results.channel.location.country;
+        array1[4] = parse.query.results.channel.astronomy.sunrise;
+        array1[5] = parse.query.results.channel.astronomy.sunset;
+        array1[6] = parse.query.results.channel.item.forecast[0].high;
+        array1[7] = parse.query.results.channel.item.forecast[0].low;
+        array1[8] = parse.query.results.channel.item.condition.temp;
+        array1[9] = parse.query.results.channel.item.condition.text;
+        $("#array2").text(array1[0]);
+        $("#array3").text(array1[1]+",");
+        $("#array4").text(array1[2]+",");
+        $("#array5").text(array1[3]);
+        $("#array6").text("Sunrise: "+array1[4]);
+        $("#array7").text("Sunset: "+array1[5]);
+        $("#array8").text("Current Temp: "+(parseInt(array1[8])-32)*0.5556+"'C");
+        $("#array9").text("High: "+(parseInt(array1[6])-32)*0.5556+"'F");
+        $("#array10").text("Low: "+(parseInt(array1[7])-32)*0.5556+"'F");
+        $("#array11").text(array1[9]);
+    })
+    })
+}
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
